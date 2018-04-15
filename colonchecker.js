56802a76ecc8da90b0a7daa9d086167ed276471f
apple502j <33279053+apple502j@users.noreply.github.com>
2018-04-15 10:17:12 +0900

    Update: Fix code so that it won't make loop

--- colonchecker.js
+++ colonchecker.js
@@ -9,8 +9,10 @@
 
 function checkColon(e){
     var nameColon = decodeURIComponent(document.URL);
-    nameColon = nameColon.replace("%3A",":").replace("%3a",":");
-    location.href = nameColon;
+    if (nameColon.toLowerCase().indexOf("%3a") > -1){
+        nameColon = nameColon.replace("%3A",":").replace("%3a",":");
+        location.href = nameColon;
+    }
 }
 
 window.addEventListener("load",checkColon);
